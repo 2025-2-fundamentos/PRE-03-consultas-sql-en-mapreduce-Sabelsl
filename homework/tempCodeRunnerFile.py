059c7946e905707b039026b65a8bd79983dd6b6f
Sara <ssuarezlo@unal.edu.co>
2025-10-01 10:18:48 -0500

    pre 03 v2

--- homework/tempCodeRunnerFile.py
+++ homework/tempCodeRunnerFile.py
@@ -1,1 +1,16 @@
-4
+
+def mapper_query_1(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append(
+                (index, row.strip() + ",tip_rate")
+            )
+        else:
+            row_values = row.strip().split(",")
+            total_bill = float(row_values[0])
+            tip = float(row_values[1])
+            tip_rate = tip / total_bill
+            result.append((index, row.strip() + "," + str(tip_rate)))
+    return result
